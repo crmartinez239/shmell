fdd7f7b468847dd041dd7555995a68f31b6c8c0f
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-11 09:42:03 -0400

    Add identifer test

--- lang/lexer_test.go
+++ lang/lexer_test.go
@@ -13,14 +13,45 @@ func TestReadTokenType(t *testing.T) {
 	assert := func(t *testing.T, got, want TokenType) {
 		t.Helper()
 		if got != want {
-			t.Errorf("got: %q - want: %q", got, want)
+			t.Errorf("got: %s - want: %s", typeToString(got), typeToString(want))
 		}
 	}
 
-	t.Run("Preprocessor token", func(t *testing.T) {
+	t.Run("Preprocessor (Bang) token", func(t *testing.T) {
 		got := lexer.ReadToken().Type
 		want := Bang
 		assert(t, got, want)
 	})
 
+	t.Run("Identifier token", func(t *testing.T) {
+		got := lexer.ReadToken().Type
+		want := Ident
+		assert(t, got, want)
+	})
+}
+
+func typeToString(t TokenType) string {
+	switch t {
+	case Ident:
+		return "Ident"
+	case Rparen:
+		return "Rparen"
+	case Lparen:
+		return "Lparen"
+	case Semi:
+		return "Semi-Colon"
+	case Colon:
+		return "Colon"
+	case Comma:
+		return "Comma"
+	case Bslash:
+		return "\\"
+	case Dot:
+		return "."
+	case Hash:
+		return "#"
+	case Bang:
+		return "!"
+	}
+	return "Undefined"
 }
