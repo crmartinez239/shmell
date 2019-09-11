1f48899a11f0460fee9ff14dde10fdc6d8b4e21b
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-11 15:20:17 -0400

    Initial code

--- shm/parser.go
+++ shm/parser.go
@@ -1,11 +1,27 @@
 package shm
 
+type ParserState int
+
+const (
+	Initial ParserState = iota
+	Preprocessor
+)
+
 // Parser undefined
 type Parser struct {
-	lexer *Lexer
+	lexer *Lexer,
+	currentState ParserState
 }
 
 // NewParser undefined
 func NewParser(lexer *Lexer) *Parser {
-	return &Parser{lexer}
+	return &Parser{lexer, Initial}
+}
+
+func (p *Parser) ParsePreprocessor() (error) {
+	for {
+		if p.currentState == Initial {
+			
+		}
+	}
 }
