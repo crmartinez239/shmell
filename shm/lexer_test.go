6fea39aa7a77069ef1bfe6c5af9e35e8227aeff0
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-12 15:35:31 -0400

    Refactor TestReadToken to TestReadRuneToken

--- shm/lexer_test.go
+++ shm/lexer_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 )
 
-func TestReadTokenType(t *testing.T) {
-	lexer, err := NewLexer("test.shm")
+func TestReadRuneToken(t *testing.T) {
+	lexer, err := NewLexer("test/runetoken.shm")
+	defer lexer.Close()
+
 	if err != nil {
 		t.Errorf("Could not create new lexer\nError: %q", err)
 	}
@@ -17,27 +19,45 @@ func TestReadTokenType(t *testing.T) {
 		}
 	}
 
-	t.Run("Preprocessor (Bang) token", func(t *testing.T) {
-		got := lexer.ReadToken().Type
-		want := Bang
+	t.Run("Lparen token", func(t *testing.T) {
+		got := lexer.ReadRuneToken().Type
+		want := Lparen
 		assert(t, got, want)
 	})
 
-	t.Run("Identifier token", func(t *testing.T) {
-		got := lexer.ReadToken().Type
-		want := Ident
+	t.Run("Rparen token", func(t *testing.T) {
+		got := lexer.ReadRuneToken().Type
+		want := Rparen
+		assert(t, got, want)
+	})
+
+	t.Run("Colon token", func(t *testing.T) {
+		got := lexer.ReadRuneToken().Type
+		want := Colon
 		assert(t, got, want)
 	})
 
 	t.Run("Semi token", func(t *testing.T) {
-		got := lexer.ReadToken().Type
+		got := lexer.ReadRuneToken().Type
 		want := Semi
 		assert(t, got, want)
 	})
 
+	t.Run("Bslash token", func(t *testing.T) {
+		got := lexer.ReadRuneToken().Type
+		want := Bslash
+		assert(t, got, want)
+	})
+
 	t.Run("EOL token", func(t *testing.T) {
-		got := lexer.ReadToken().Type
+		got := lexer.ReadRuneToken().Type
 		want := EOL
+		assert(t, got, want)
+	})
+
+	t.Run("Undefined token", func(t *testing.T) {
+		got := lexer.ReadRuneToken().Type
+		want := Undefined
 		assert(t, got, want)
 	})
 }
