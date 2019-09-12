a14bf40ac72f5bacc16ca6f0995c79bd28dc2438
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-12 16:03:08 -0400

    Add ParserError interface

--- shm/parser.go
+++ shm/parser.go
@@ -1,28 +1,42 @@
 package shm
 
-type ParserState int
+// ParserError error interface
+type ParserError struct {
+	err   string
+	token Token
+}
+
+// Token that casued the parser error
+func (e *ParserError) Token() *Token {
+	return &e.token
+}
+
+func (e *ParserError) Error() string {
+	return e.err
+}
+
+type parserState int
 
 const (
-	Initial ParserState = iota
-	Preprocessor
+	preprocessor parserState = iota
 )
 
 // Parser undefined
 type Parser struct {
 	lexer        *Lexer
-	currentState ParserState
+	currentState parserState
 }
 
 // NewParser undefined
 func NewParser(lexer *Lexer) *Parser {
-	return &Parser{lexer, Initial}
+	return &Parser{lexer, preprocessor}
 }
 
 func (p *Parser) Parse() error {
 	for {
-		if p.currentState == Initial {
+		if p.currentState == preprocessor {
 			// Look for preprocessor statements
-			token := p.lexer.ReadToken()
+			token := p.lexer.ReadRuneToken()
 
 			if token.Type == Bang {
 				p.parsePreprocessorStatement()
