793397b09732aa3009532ea0258cdbf281af1178
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-12 23:57:19 -0400

    Add parsePreprocessorStatement logic

--- shm/parser.go
+++ shm/parser.go
@@ -3,12 +3,12 @@ package shm
 // ParserError error interface
 type ParserError struct {
 	err   string
-	token Token
+	token *Token
 }
 
 // Token that casued the parser error
 func (e *ParserError) Token() *Token {
-	return &e.token
+	return e.token
 }
 
 func (e *ParserError) Error() string {
@@ -35,18 +35,34 @@ func NewParser(lexer *Lexer) *Parser {
 // Parse parses the current Lexer stream
 func (p *Parser) Parse() error {
 	for {
+
 		if p.currentState == preprocessor {
 			// Look for preprocessor statements
-			token := p.lexer.ReadRuneToken()
+			token, tknErr := p.lexer.ReadRuneToken()
 
-			if token.Type == Bang {
-				p.parsePreprocessorStatement()
+			if tknErr != nil {
+				// lexer will only return system errors
+				return tknErr
 			}
 
+			if token.Type != Bang {
+				// Set new parser state
+				p.lexer.UnreadRune()
+				continue
+			}
+
+			parseErr := p.parsePreprocessorStatement()
 		}
+
 	}
 }
 
 func (p *Parser) parsePreprocessorStatement() error {
+	tag, tagErr := p.lexer.ReadTagToken()
 
+	if tag.Tag == None {
+		return &ParserError{"Expected: tag", tag}
+	}
+
+	return nil
 }
