a08319ece54ca5110d0779e055ab87680aad002d
Christopher Martinez <crmartinez239@protonmail.com>
2019-09-14 01:07:59 -0400

    Add parsePreprocessorStatement logic

--- shm/parser.go
+++ shm/parser.go
@@ -38,31 +38,54 @@ func (p *Parser) Parse() error {
 
 		if p.currentState == preprocessor {
 			// Look for preprocessor statements
-			token, tknErr := p.lexer.ReadRuneToken()
+			token, tknErr := p.lexer.PeekRuneToken()
 
 			if tknErr != nil {
+				// Empty AST
 				// lexer will only return system errors
 				return tknErr
 			}
 
 			if token.Type != Bang {
 				// Set new parser state
-				p.lexer.UnreadRune()
 				continue
 			}
 
-			parseErr := p.parsePreprocessorStatement()
+			// parseErr := p.parsePreprocessorStatement()
 		}
 
 	}
 }
 
 func (p *Parser) parsePreprocessorStatement() error {
+	// ![tag]
+	// ![tag] [attribute] = [value]
+	p.lexer.ReadRuneToken() // the Bang token
 	tag, tagErr := p.lexer.ReadTagToken()
+
+	if tagErr != nil {
+		return tagErr
+	}
 
 	if tag.Tag == None {
 		return &ParserError{"Expected: tag", tag}
 	}
+
+	peek, _ := p.lexer.PeekRuneToken()
+	if peek.Type == EOL {
+		// AST logic goes here
+		p.lexer.ReadRuneToken()
+		return nil
+	}
+
+	if err := p.parseAttributes(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Parser) parseAttributes() error {
 
 	return nil
 }
